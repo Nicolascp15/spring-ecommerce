60b397f3e23d236e956c863d0b1bf7811ccf086c
Nicolas Castro Patiño <nicolascastropatino@gmail.com>
2025-03-07 14:20:29 +0100

    mostrar la vista del producto al usuario

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.curso.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.services.ProductoService;
@@ -13,6 +16,7 @@ import com.curso.ecommerce.services.ProductoService;
 @RequestMapping("/")
 public class HomeController 
 {
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);//variable log para hacer pruebas por consola
 	@Autowired
 	private ProductoService productoService;//variable para obtener los productos de la BBDD y mostrarlos al usuario
 	@GetMapping("/")
@@ -21,5 +25,13 @@ public class HomeController
 
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
+	}
+	
+	//metodo que nos va a llevar desde el boton " ver producto" a la vista producto home 
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id)
+	{
+		log.info("Id Producto Enviado Como Parametro {}",id);
+		return "usuario/productohome";
 	}
 }
