973eb5b484339aedb7aa3ac9a45ff57983055e63
Nicolas Castro Patiño <nicolascastropatino@gmail.com>
2025-03-09 18:30:29 +0100

    añadir el nombre,descripcion,precio y imagen a la descripcion del producto

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.services.ProductoService;
 
 //esta clase tendra la logica para mostrar todos los productos al usuario
@@ -29,9 +32,17 @@ public class HomeController
 	
 	//metodo que nos va a llevar desde el boton " ver producto" a la vista producto home 
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id)
+	public String productoHome(@PathVariable Integer id,Model model)//variable Model para llevar informacion a la vista
 	{
 		log.info("Id Producto Enviado Como Parametro {}",id);
+		Producto producto =new Producto();//declarar variable de objeto Producto para buscarlo en la BBDD
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();//para obterner el producto de la base de datos
+		
+		model.addAttribute("producto",producto);//mandar el producto a la vista
+		
 		return "usuario/productohome";
 	}
+	
+	
 }
