85d3af49f55bc3e65b8029042a6cfbcd66bda2f0
Nicolas Castro Patiño <nicolascastropatino@gmail.com>
2025-03-10 12:46:11 +0100

    redireccion al carrito de compras

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.services.ProductoService;
@@ -43,6 +45,15 @@ public class HomeController
 		
 		return "usuario/productohome";
 	}
+	//peticion tipo postmapping para redireccionar al carrito
+	
+	@PostMapping("/cart")
+	public String addCart() {
+	  
+	    
+	    return "usuario/carrito"; 
+	}
+
 	
 	
 }
