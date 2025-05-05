e173a8f40c4923009dccdf737df2e212dddd5e1c
Nicolas Castro Patiño <nicolascastropatino@gmail.com>
2025-05-05 12:50:27 +0200

    funcionalkidad para ver los detalles de una orden en admin

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -2,12 +2,16 @@ package com.curso.ecommerce.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.services.IOrdenService;
 import com.curso.ecommerce.services.IUsuarioService;
@@ -22,6 +26,8 @@ public class AdministradorController {
 	private IUsuarioService usuarioService;//variable para acceddr a la informacion de los usuarios desde el ADMIN
 	@Autowired
 	private IOrdenService ordenService;//variable para acceder a los metodos CRUD de las ordenes 
+	
+	private Logger logg = LoggerFactory.getLogger(AdministradorController.class);
 	
 	//metodo que nos redirecciona hacia el archivo html home
 	//modificamos este metodo para que muestre los productos en la home
@@ -45,6 +51,17 @@ public class AdministradorController {
 	{
 		model.addAttribute("ordenes", ordenService.findAll());
 		return "administrador/ordenes";
+	}
+	
+	//metodo para mapear la peticion cuando estemos en ver detalle
+	@GetMapping("/detalle/{id}")
+	public String detalle(Model model , @PathVariable Integer id)
+	{
+		logg.info("ID de la orden {}",id);
+		Orden orden = ordenService.findById(id).get();
+		
+		model.addAttribute("detalle", orden.getDetalle());
+		return "administrador/detalleorden";
 	}
 	
 }
