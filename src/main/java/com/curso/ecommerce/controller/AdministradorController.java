2f13967d72a7ccada9170815b36132104156a054
Nicolas Castro Patiño <nicolascastropatino@gmail.com>
2025-05-05 11:47:53 +0200

    funcionalkidad para ver las ordenes por el ADMIN

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.services.IOrdenService;
 import com.curso.ecommerce.services.IUsuarioService;
 import com.curso.ecommerce.services.ProductoService;
 
@@ -19,6 +20,8 @@ public class AdministradorController {
 	private ProductoService productoService;//para acceder a los productos
 	@Autowired
 	private IUsuarioService usuarioService;//variable para acceddr a la informacion de los usuarios desde el ADMIN
+	@Autowired
+	private IOrdenService ordenService;//variable para acceder a los metodos CRUD de las ordenes 
 	
 	//metodo que nos redirecciona hacia el archivo html home
 	//modificamos este metodo para que muestre los productos en la home
@@ -29,12 +32,19 @@ public class AdministradorController {
 		model.addAttribute("productos",productos);//este metodo lo pasa a la vista home
 		return "administrador/home";
 	}
-	
+	//metodo para que nos redireccione a a la vista /administrador/usuarios
 	@GetMapping("/usuarios")
 	public String usuarios(Model model)
 	{
 		model.addAttribute("usuarios", usuarioService.findAll());
 		return "administrador/usuarios";
+	}
+	
+	@GetMapping("/ordenes")
+	public String ordenes(Model model)
+	{
+		model.addAttribute("ordenes", ordenService.findAll());
+		return "administrador/ordenes";
 	}
 	
 }
